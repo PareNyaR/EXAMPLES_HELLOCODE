b68b67643a5a8eaaef29551f57bb04ddd81157aa
YarChi-Pi <52123163+PareNyaR@users.noreply.github.com>
2021-11-11 21:20:35 +0300

    add example007 magic

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿Console.Clear();
+// Console.SetCursorePosition(10, 1);
+// Console.WriteLine("+");
+
+int xa = 40;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 80;
+int yc = 30;
+
+Console.SetCursorePosition(xa,ya);
+Console.WriteLine("+");
+
+Console.SetCursorePosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorePosition(xc, yc);
+Console.WriteLine("+");
+
+
+int x = xa, y = xb;
+
+int count = 10;
+
+while(count<10000)
+{
+    int what = new Random().Next(0, 3); // [0,3)0 1 2
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + xy) / 2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorePosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1;
+}                
