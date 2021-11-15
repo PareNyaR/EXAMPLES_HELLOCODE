3dfbb4cea049c7df4c51101b96042bd61b9e4275
YarChi-Pi <52123163+PareNyaR@users.noreply.github.com>
2021-11-15 21:16:09 +0300

    add Example11 ArrayLibrary

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,4 +1,4 @@
-﻿int max (int arg1,int arg2, int arg3)
+﻿int Max (int arg1,int arg2, int arg3)
 {
     int result=arg1;
     if (arg2>result) result = arg2;
@@ -11,11 +11,13 @@ int c1 = 39;
 int a2 = 112;
 int b2 = 223; 
 int c2 = 33;
-int a3 = 55;
+int a3 = 5555;
 int b3 = 223;
 int c3 = 313;
 
-int max1 = max(a1, b1, c1);
-int max2 = max(a2, b2, c2);
-int max3 = max(a3, b3, c3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 Console.WriteLine(max);
