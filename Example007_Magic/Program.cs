41d26f789e36b49bdb5759560b13d93702aafc80
YarChi-Pi <52123163+PareNyaR@users.noreply.github.com>
2022-01-07 14:50:05 +0300

    add edit example007

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -9,7 +9,7 @@ int yb = 30;
 int xc = 80;
 int yc = 30;
 
-Console.SetCursorePosition(xa,ya);
+Console.SetCursorePosition(xa, ya);
 Console.WriteLine("+");
 
 Console.SetCursorePosition(xb, yb);
@@ -19,32 +19,31 @@ Console.SetCursorePosition(xc, yc);
 Console.WriteLine("+");
 
 
-int x = xa, y = xb;
+//int x = xa, y = xb;
 
-int count = 10;
+//int count = 10;
 
-while(count<10000)
-{
-    int what = new Random().Next(0, 3); // [0,3)0 1 2
-    if(what == 0)
-    {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
-    }
+//while(count<10000)
+//{
+  //  int what = new Random().Next(0, 3); // [0,3)0 1 2
+  //  if(what == 0)
+  //  {
+  //      x = (x + xa) / 2;
+   //     y = (y + ya) / 2;
+   // }
 
-    if(what == 1)
-    {
-        x = (x + xb) / 2;
-        y = (y + xy) / 2;
-    }
+    //if(what == 1)
+    //    x = (x + xb) / 2;
+   //     y = (y + xy) / 2;
+  //  }
 
-    if(what == 2)
-    {
-        x = (x + xc) / 2;
-        y = (y + yc) / 2;
-    }
+  //  if(what == 2)
+  //  {
+  //      x = (x + xc) / 2;
+   //     y = (y + yc) / 2;
+    //}
 
-    Console.SetCursorePosition(x, y);
-    Console.WriteLine("+");
-    count = count + 1;
-}                
+   // Console.SetCursorePosition(x, y);
+    //Console.WriteLine("+");
+    //count = count + 1;
+//}                
